b036bb4976baf4e6c000432a306e53cbedbc568d
1005gar <you@example.com>
2025-03-25 10:42:47 +0300

    refactoring and add lr3-exc1

---
+++ lr3-exc1.py
@@ -0,0 +1,42 @@
+import pygame
+import time
+
+pygame.init()
+window_width = 800
+window_height = 600
+fon = 'fon.png'
+
+window = pygame.display.set_mode([window_width, window_height])
+pygame.display.set_caption("Игра v1.0")
+
+speed = 0
+sdvig_fona = 0
+
+img1 = pygame.image.load(fon)
+back_fon = pygame.transform.scale(img1, (window_width, window_height))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed = 5
+            elif event.key == pygame.K_RIGHT:
+                speed = -5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                speed = 0
+
+    sdvig_fona = (sdvig_fona + speed) % window_width
+    window.blit(back_fon, (sdvig_fona, 0))
+    if sdvig_fona != 0:
+        window.blit(back_fon, (sdvig_fona - window_width, 0))
+    elif speed < 0:
+        window.blit(back_fon, (sdvig_fona + window_width, 0))
+
+    pygame.display.update()
+    time.sleep(0.02)
+
+pygame.quit()
